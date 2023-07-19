55ccabcd77993de9929b90d39765f49c76bc1b22
bookland <rratcliffe57@gmail.com>
2023-07-18 21:04:24 -0500

    first commit

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,40 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import * as Ably from 'ably';
 
-type Data = {
-  name: string
-}
 
-export default function handler(
+
+
+
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  async function fetchData() {
+    try {
+      const response = await fetch('https://rest.ably.io/channels', {
+        method: 'GET',
+        headers: {
+          'Authorization': 'Basic ' + btoa(process.env.ABLY_API_KEY as string)
+        }
+      });
+      
+      const data = await response.json();
+      res.status(200).json( data )
+
+      // Handle the response data here
+    } catch (error) {
+      // Handle any errors that occurred during the request
+      console.error('Error:', error);
+    }
+  }
+
+  await fetchData();
+
+  
+  
+  
+  
+  
+
 }
